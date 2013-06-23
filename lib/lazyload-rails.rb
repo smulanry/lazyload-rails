505696411efd2a6409b0d7cc6f83916e36165b76
Ryan Smulan <ryan.smulan@gmail.com>
2013-06-23 11:48:16 -0300

    Update lazyload-rails.rb
    rename method so it does not replace the default

--- lib/lazyload-rails.rb
+++ lib/lazyload-rails.rb
@@ -32,9 +32,7 @@ module Lazyload
 end
 
 ActionView::Helpers::AssetTagHelper.module_eval do
-  alias :x_image_tag :image_tag
-
-  def image_tag(*attrs)
+  def lazy_image_tag(*attrs)
     placeholder = Lazyload::Rails.configuration.placeholder
     img = Nokogiri::HTML::DocumentFragment.parse(x_image_tag(*attrs)).at_css("img")
     src = img["src"]
